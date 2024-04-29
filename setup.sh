55101f813290ecddbab8c50d12a7f54594c363bf
PGr <przemyslaw.grzyska@solutionsca.pl>
2024-04-29 13:28:00 +0200

    setup updated

--- setup.sh
+++ setup.sh
@@ -2,7 +2,5 @@
 rsync -r sys_files/etc/* /etc/
 rsync -r sys_files/usr/* /usr/
 for user in `ls -1 /home/`; do
-    # su -c "cp -r ./sys_files/etc/skel/.* /home/$user/" $user
-    su -c "rsync -ar sys_files/etc/skel/* /home/$user/" $user
-    # su -c "rsync -r sys_files/etc/skel/.* /home/$user/" $user
+    su -c "rsync -ar sys_files/etc/skel/ /home/$user/" $user
 done
